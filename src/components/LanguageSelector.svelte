67106b8cadc6b33178e818223d1d41b4acfcc813
David Don Young Kim <koyykdy@gmail.com>
2022-04-30 14:06:57 -0700

    progress towards TDD svelte practice app MVP

--- src/components/LanguageSelector.svelte
+++ src/components/LanguageSelector.svelte
@@ -2,8 +2,8 @@
   import { locale } from "svelte-i18n";
 </script>
 
-<img src="https://www.countryflags.io/tr/flat/24.png" alt="Turkish Flag" title="Türkçe" on:click={() => locale.set("tr")} />
-<img src="https://www.countryflags.io/us/flat/24.png" alt="English Flag" title="English" on:click={() => locale.set("en")} />
+<img src="../../public/assets/turkeyflag.png" alt="Turkish Flag" title="Türkçe" on:click={() => locale.set("tr")} />
+<img src="../../public/assets/ukflag.png" alt="English Flag" title="English" on:click={() => locale.set("en")} />
 
 <style>
   img {
